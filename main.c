3879073d078f5091a12d5ec484cfa0ca66f38699
破壊力 <74363032+re-roll@users.noreply.github.com>
2021-11-28 16:34:06 +0100

    Update main.c

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 
 #define max_len 6
+#define max_len_u 8
 
 typedef struct
 {
@@ -9,10 +10,20 @@ typedef struct
     int y;
 } rel_t;
 
+typedef struct
+{
+    int x;
+} uni_t;
+
 void checking(rel_t *rel, int *a, int *b)
 {
     *a = rel -> x;
     *b = rel -> y;
+}
+
+void checking_u(uni_t *uni, int *a)
+{
+    *a = uni -> x;
 }
 
 int isRef(rel_t *rel)
@@ -46,7 +57,7 @@ int isSym(rel_t *rel)
 
     rel_t *rel_buff;
 
-    for (int i = 0; i <= max_len - 1; i++)
+    for (int i = 0; i < max_len; i++)
     {
         rel_buff = &rel[i];
         checking(rel_buff, &a, &b);
@@ -54,7 +65,7 @@ int isSym(rel_t *rel)
         a_buff = a;
         b_buff = b;
 
-        for (int j = 0; j <= max_len - 1; j++)
+        for (int j = 0; j < max_len; j++)
         {
             rel_buff = &rel[j];
             checking(rel_buff, &a, &b);
@@ -89,7 +100,7 @@ int isAntisym(rel_t *rel)
 
     rel_t *rel_buff;
 
-    for (int i = 0; i <= max_len - 1; i++)
+    for (int i = 0; i < max_len; i++)
     {
         rel_buff = &rel[i];
         checking(rel_buff, &a, &b);
@@ -97,7 +108,7 @@ int isAntisym(rel_t *rel)
         a_buff = a;
         b_buff = b;
 
-        for (int j = 0; j <= max_len - 1; j++)
+        for (int j = 0; j < max_len; j++)
         {
             rel_buff = &rel[j];
             checking(rel_buff, &a, &b);
@@ -142,7 +153,7 @@ int isTran(rel_t *rel)
 
     rel_t *rel_buff;
 
-    for (int i = 0; i <= max_len - 1; i++)
+    for (int i = 0; i < max_len; i++)
     {
         rel_buff = &rel[i];
         checking(rel_buff, &a, &b);
@@ -153,7 +164,7 @@ int isTran(rel_t *rel)
         x = a;
         z = b;
 
-        for (int j = 0; j <= max_len - 1; j++)
+        for (int j = 0; j < max_len; j++)
         {
             rel_buff = &rel[j];
             checking(rel_buff, &a, &b);
@@ -166,7 +177,7 @@ int isTran(rel_t *rel)
                     cnt_b++;
                     break;
                 }
-                for (int k = 0; k <= max_len - 1; k++)
+                for (int k = 0; k < max_len; k++)
                 {
                     rel_buff = &rel[k];
                     checking(rel_buff, &x, &z);
@@ -192,87 +203,139 @@ int isFun(rel_t *rel)
     int b = 0;
 
     int a_buff = 0;
-    int b_buff = 0;
 
     rel_t *rel_buff;
 
-    for (int i = 0; i <= max_len - 1; i++)
+    for (int i = 0; i < max_len; i++)
     {
         rel_buff = &rel[i];
         checking(rel_buff, &a, &b);
         a_buff = a;
-        b_buff = b;
-        for (int j = 0; j <= max_len - 1; j++)
+        int cnt = 0;
+        for (int j = 0; j < max_len; j++)
         {
             rel_buff = &rel[j];
             checking(rel_buff, &a, &b);
-            if ((a == a_buff) && (b != b_buff) && (a != b))
-                return 0;
+            if (a == a_buff)
+                cnt++;
         }
+        if (cnt != 1)
+            return 0;
     }
 
     return 1;
 }
 
-int isInj(rel_t *rel)
+void domain(rel_t *rel)
 {
     int a = 0;
     int b = 0;
 
-    int cnt = 0;
-
-    int a_buff = 0;
-    int b_buff = 0;
-
     rel_t *rel_buff;
+
     for (int i = 0; i <= max_len - 1; i++)
     {
         rel_buff = &rel[i];
         checking(rel_buff, &a, &b);
-        a_buff = a;
+        printf("%d ", a);
+    }
+
+    printf("\n");
+}
+
+void codomain(rel_t *rel)
+{
+    int a = 0;
+    int b = 0;
+
+    rel_t *rel_buff;
+
+    for (int i = 0; i <= max_len - 1; i++)
+    {
+        rel_buff = &rel[i];
+        checking(rel_buff, &a, &b);
+        printf("%d ", b);
+    }
+
+    printf("\n");
+}
+
+int isInj(rel_t *rel)
+{
+    if (!(isFun(rel)))
+        return 0;
+    
+    int a = 0;
+    int b = 0;
+
+    int b_buff = 0;
+    int a_buff = 0;
+
+    rel_t *rel_buff;
+    for (int i = 0; i < max_len; i++)
+    {
+        rel_buff = &rel[i];
+        checking(rel_buff, &a, &b);
         b_buff = b;
-        for (int j = 0; j <= max_len - 1; j++)
+        a_buff = a;
+        for (int j = 0; j < max_len; j++)
         {
+            if (i == j)
+                break;
             rel_buff = &rel[j];
             checking(rel_buff, &a, &b);
-            if ((b == b_buff) && (a != a_buff))
-                return 0;
-            else
-                cnt++;
+            if (b == b_buff)
+                if (a != a_buff)
+                   return 0;
         }
     }
 
-    if (cnt == max_len*max_len)
+return 1;
+}
+
+int isSur(rel_t *rel, uni_t *uni)
+{
+    if (!(isFun(rel)))
+        return 0;
+    
+    int a = 0;
+    int b = 0;
+
+    int x = 0;
+
+    int cnt = 0;
+
+    rel_t *rel_buff;
+    uni_t *uni_buff;
+    for (int i = 0; i < max_len_u; i++)
+    {
+        rel_buff = &rel[i];
+        checking(rel_buff, &a, &b);
+
+        uni_buff = &uni[i];
+        checking_u(uni_buff, &x);
+
+        for (int j = 0; j < max_len; j++)
+        {
+            rel_buff = &rel[j];
+            checking(rel_buff, &a, &b);
+            if (b == x)
+            {
+                cnt++;
+                break;
+            }
+        }
+    }
+
+    if (cnt == max_len)
         return 1;
 
     return 0;
 }
 
-int isSur(rel_t *rel)
+int isBij(rel_t *rel, uni_t *uni)
 {
-    int a = 0;
-    int b = 0;
-
-    int cnt = 0;
-
-    int b_buff = 0;
-
-    rel_t *rel_buff;
-    for (int i = 0; i <= max_len - 1; i++)
-    {
-        rel_buff = &rel[i];
-        checking(rel_buff, &a, &b);
-        b_buff = b;
-        for (int j = 0; j <= max_len - 1; j++)
-        {
-            rel_buff = &rel[j];
-            checking(rel_buff, &a, &b);
-            if (b == b_buff)
-                cnt++;
-        }
-    }
-
-    if (cnt != 0)
+    if ((isInj(rel)) && (isSur(rel, uni)))
         return 1;
 
     return 0;
@@ -280,7 +343,8 @@ int isSur(rel_t *rel)
 
 int main()
 {
-    rel_t rel1[max_len] = { {3,1}, {3,1}, {0,0}, {5,5}, {7,2}, {2,6}};
+    rel_t rel1[max_len] = { {3,8}, {6,9}, {0,0}, {5,5}, {7,2}, {2,6}};
+    uni_t uni[max_len_u] = {{3}, {8}, {6}, {9}, {0}, {5}, {7}, {2}};
     
     if (isRef (rel1))
         printf("reflexive\n");
@@ -297,11 +361,18 @@ int main()
     if (isFun(rel1))
         printf("function\n");
 
+    domain(rel1);
+
+    codomain(rel1);
+
     if (isInj(rel1))
         printf("injection\n");
 
-    if (isSur(rel1))
+    if (isSur(rel1, uni))
         printf("surjection\n");
+
+    if (isBij(rel1, uni))
+        printf("bijection\n");
 
     return 0;
 }
